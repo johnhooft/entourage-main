b6627d1950b943d3b0f089b9eda129d799b45c48
johnhooft <110422043+johnhooft@users.noreply.github.com>
2024-08-24 14:47:05 -0700

    Typo

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Home from '../components/home';
+import Home from '../components/Home';
 import Site from '../components/Site';
 
 export default function Index() {
