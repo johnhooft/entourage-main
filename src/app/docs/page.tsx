5377333689fce8c52255b2225b19335072114e63
johnhooft <110422043+johnhooft@users.noreply.github.com>
2024-09-08 19:41:50 -0700

    Small style changes

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -6,12 +6,6 @@ import React from 'react';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
-    const router = useRouter();
-
-    const handleGenerateClick = () => {
-        router.push('/');
-    };
-
     return (
         <UserDocs />
     );
--- src/app/modules/corp/Footer.tsx
+++ src/app/modules/corp/Footer.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className="w-full bg-black text-entourage-orange py-8">
       <div className="container mx-auto px-4">
-        <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:justify-between items-center text-xl">
+        <div className="flex flex-col space-y-4 md:flex-row md:space-y-0 md:justify-between items-center text-lg">
           <div className="flex flex-col space-y-4 items-center md:flex-row md:space-y-0 md:space-x-12">
             <Link href="/docs" className="hover:text-orange-500 transition-colors">
               User Docs
